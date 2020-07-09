7fd042d413a544161e238753d19a534884467e0d
Logeshwaren <68047002+Logeshwaren@users.noreply.github.com>
2020-07-09 11:19:55 +0530

    Add files via upload

---
+++ HTML_CSS_JavaScript/script.js
@@ -0,0 +1,92 @@
+// declaring or creating a variables
+// var, let, const(can't change the value later once we declared).
+// declaring a variable in single line.
+
+/*
+
+let fName = 'Logeshwaran'; //string
+let lName  = 'H';
+// declaring a multiple variables in single line
+let age = 27, //number
+    isHeWorking = true, // boolean
+    companyName = 'DV Signage',
+    role = 'Client lead';
+
+console.log(fName + ' ' + lName + ' is ' + age + ' years old.' + ' Is he workoing? '+ isHeWorking + ', Yeah he is working in ' + companyName + ' as a ' + role +'.');
+
+ document.getElementById('coding').innerHTML += fName + ' ' + lName + ' is ' + age + ' years old.' + ' Is he workoing? '+ isHeWorking + ', Yeah he is working in ' + companyName + ' as a ' + role +'.';
+
+*/
+
+ // Data Types => 
+/*
+Primitive Data types:
+    String, Number, Boolean, Undefined, 
+    
+ Complex Data Types:   
+    Object, Array, Function, Symbol
+*/
+
+/*
+Operators:
+
+1. Arithmatic Operators:
+    addition, (5 + 2) => 7, (Hello + ' ' + World! = Hello World!)
+    subtraction, (5 - 2) => 3
+    multiplication, (5 * 2) => 10
+    division, / (10 / 2) => 5
+    modulous, % (5 % 2) => 1
+    exponential, ** (5 ** 2) => 25
+    increment, ++ (5++)
+    decrement, -- (5--)
+
+2. Logical Operators: 
+    and (&&), (1*1=1, 1*0=0, 0*1=0, 0*0=0)
+    or (||), (1*1=1, 1*0=1, 0*1=1, 0*0=0)
+    not (!), (0=1, 1=0)
+*/
+
+/*
+let num1 = 8,
+    num2 = 6,
+    num3 = 10,
+    num4 = 3;
+
+let num5 = 10;
+num5++;
+
+let num6 = 7;
+num6--;
+
+let addition = num1 + num2; 
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+let exponential = num3 ** num4; 
+let increment = num5;
+let decrement = num6;
+
+console.log('addition: '+ addition + ',' + ' subtraction: ' + subtraction +','+ ' multiplication: '+ multiplication + ','+ ' division: ' + division + ',' + ' modulus: ' + modulus + ','+ ' exponential: ' + exponential + ',' + ' increment: ' + increment + ',' + ' decrement: '+ decrement + '.')
+
+document.getElementById('coding').innerHTML += 'addition: '+ addition + '<br>' + ' subtraction: ' + subtraction +'<br>'+ ' multiplication: '+ multiplication + '<br>'+ ' division: ' + division + '<br>' + ' modulus: ' + modulus + '<br>'+ ' exponential: ' + exponential + '<br>' + ' increment: ' + increment + '<br>' + ' decrement: '+ decrement;
+*/
+
+/*let fatherName = 'Hari Krishanan',
+    fatherAge = 60;
+
+let motherName = 'Kalai Selvi',
+    motherAge = 48;
+ 
+let sonName = 'Logeshwaran',
+    sonAge = 27;
+
+let daughterName = 'Priyadharshini',
+    daughterAge = 17;
+
+console.log (`In our family, we are four. My dad name is ${fatherName} and he is ${fatherAge} years old. My Mom name is ${motherName} and she is ${motherAge} years old. Myself ${sonName} and I'm ${sonAge} years old. My sister name is ${daughterName} and she is ${daughterAge} years old. `)     
+
+document.getElementById('coding').innerHTML += `In our family, we are four. My dad name is ${fatherName} and he is ${fatherAge} years old. My Mom name is ${motherName} and she is ${motherAge} years old. Myself ${sonName} and I'm ${sonAge} years old. My sister name is ${daughterName} and she is ${daughterAge} years old. `;
+*/
+
+
